cd4f1d2c3f3b1da534b520f736c08ab86b0bc8c8
Vanshika <vanshipahwa99@gmail.com>
2021-09-11 22:29:39 +0530

    Mongoose population

--- models/dishes.js
+++ models/dishes.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+
 var commentSchema = new Schema({
     rating:  {
         type: Number,
@@ -11,9 +16,9 @@ var commentSchema = new Schema({
         type: String,
         required: true
     },
-    author:  {
-        type: String,
-        required: true
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 }, {
     timestamps: true
@@ -29,8 +34,29 @@ const dishSchema = new Schema({
         type: String,
         required: true
     },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
     comments:[commentSchema]
-},{
+}, {
     timestamps: true
 });
 
