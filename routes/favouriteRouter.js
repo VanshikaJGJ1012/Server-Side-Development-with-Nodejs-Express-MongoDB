95920f91f0fd9e0dbb1c4355895172428471d07f
Vanshika <vanshipahwa99@gmail.com>
2021-09-12 14:51:39 +0530

    Integrating Client and Server

--- routes/favouriteRouter.js
+++ routes/favouriteRouter.js
@@ -11,16 +11,31 @@ favoriteRouter.use(bodyParser.json());
 favoriteRouter.route('/')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 .all(authenticate.verifyUser)
-.get((req, res, next)=> {
-    Favorite.find({'user': req.user._id})
-        .populate('user')
-        .populate('dishes')
-        .then((favorites) =>{
-            res.json(favorites);
-        },(err) => next(err))
-        .catch((err) => next(err));
-})
 
+.get(cors.cors, authenticate.verifyUser, (req,res,next) => {
+    Favorites.findOne({user: req.user._id})
+    .then((favorites) => {
+        if (!favorites) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            return res.json({"exists": false, "favorites": favorites});
+        }
+        else {
+            if (favorites.dishes.indexOf(req.params.dishId) < 0) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": false, "favorites": favorites});
+            }
+            else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": true, "favorites": favorites});
+            }
+        }
+
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
 .post(function (req, res, next) {
 
     Favorite.find({'user': req.user._id})
