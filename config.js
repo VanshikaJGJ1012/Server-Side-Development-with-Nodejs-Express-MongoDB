357c4f3ef6fe0f779178510dc3ab6a646b8b92b8
Vanshika <vanshipahwa99@gmail.com>
2021-09-12 13:40:27 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' :{
+        clientId:'2044833172346084',
+        clientSecret:'bc0f3c4d65176095e5bf579b94ca2296'
+    }
 }
